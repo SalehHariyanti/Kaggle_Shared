Adding predictions for images of with fixed scaling

Mask_RCNN_DSB2018/DSB2018/submit.py
@@ -559,13 +559,72 @@ def predict_tiled_model(_config, dataset, epoch = None, tile_threshold = 0, grid
                                                         datetime.datetime.now().strftime('%Y%m%d%H%M%S'), '.csv'))), ImageId, EncodedPixels)
 
 
+def predict_scaled_model(_config, dataset, epoch = None, nms_threshold = 0.3):
+    """
+    Predict in two phases:
+    1) predict as normal and estimate avg nucleus size
+    2) rescale test images according to the model's training avg nucleus size and predict again
+    """
+    # Recreate the model in inference mode
+    model = create_model(_config, epoch)
+
+    # Retrieve the avg mask size
+    assert _config.mask_size_filename is not None
+    model_mask_size = np.load(_config.mask_size_filename)
+    model_mask_size = np.mean(model_mask_size[np.logical_not(np.isnan(model_mask_size))])
+      
+    ImageId = []
+    EncodedPixels = []
+    
+    # NB: we need to predict in batches of _config.BATCH_SIZE
+    # as there are layers within the model that have strides dependent on this.
+    for i in range(0, len(dataset.image_ids), _config.BATCH_SIZE):
+         # Load image
+        images = []
+        N = 0
+        for idx in range(i, i + _config.BATCH_SIZE):
+            if idx < len(dataset.image_ids):
+                N += 1
+                images.append(dataset.load_image(dataset.image_ids[idx]))
+            else:
+                images.append(images[-1])
+
+        # Run detection
+        _r = model.detect(images, verbose=0)
+        
+        # Estimate the mask size 
+        avg_mask_size = []
+        for j in range(_config.BATCH_SIZE):                 
+            masks = _r[j]['masks'] 
+            #NB: need to resize to model input scale so that is comparable with recorded model_mask_size
+            _, window, scale, padding = utils.resize_image(images[j], min_dim = _config.IMAGE_MIN_DIM, max_dim = _config.IMAGE_MAX_DIM, padding = _config.IMAGE_PADDING)
+            resized_masks = utils.resize_mask(masks, scale, padding)
+            avg_mask_size.append(np.mean(np.sum(resized_masks, axis = (0, 1))))
+
+        # Detect again, but with images scaled according to predicted mask size
+        r = model.detect(images, verbose = 0, mask_scale = [np.sqrt(model_mask_size / x) for x in avg_mask_size])
+
+        for j, idx in enumerate(range(i, i + _config.BATCH_SIZE)):      
+
+            if j < N:   
+
+                masks = r[j]['masks'] #[H, W, N] instance binary masks
+                img_name = dataset.image_info[idx]['name']
+        
+                ImageId_batch, EncodedPixels_batch = f.numpy2encoding_no_overlap_threshold(masks, img_name, r[j]['scores'])
+                ImageId += ImageId_batch
+                EncodedPixels += EncodedPixels_batch
+
+    f.write2csv(os.path.join(submissions_dir, '_'.join(('submission_scaled', _config.NAME, str(epoch), datetime.datetime.now().strftime('%Y%m%d%H%M%S'), '.csv'))), ImageId, EncodedPixels)
+
+
 def predict_experiment(fn_experiment, fn_predict = 'predict_model'):
     _config, dataset = fn_experiment(training=False)
     globals()[fn_predict](_config, dataset)
 
 
 def main():
-    predict_experiment(train.train_resnet101_flips_all_rots_data_minimask12_detectionnms0_3)
+    predict_experiment(train.train_resnet101_flips_alldata_minimask12_double_invert_masksizes, 'predict_scaled_model')
 
 
 if __name__ == '__main__':

Mask_RCNN_DSB2018/utils.py
@@ -431,15 +431,14 @@ def resize_image(image, min_dim=None, max_dim=None, padding=False):
     return image, window, scale, padding
 
 
-def resize_image2(image, min_dim=None, max_dim=None, padding=False):
+def resize_image_scaled(image, scale, min_dim=None, max_dim=None):
     """
-    Resizes an image keeping the aspect ratio.
+    Resizes an image to a given scale.
 
-    min_dim: if provided, resizes the image such that it's smaller
-        dimension == min_dim
+    If resizing to this scale exceeds max_dim then the image is rescaled to max_dim
+
     max_dim: if provided, ensures that the image longest side doesn't
         exceed this value.
-    padding: If true, pads image with zeros so it's size is max_dim x max_dim
 
     Returns:
     image: the resized image
@@ -450,15 +449,10 @@ def resize_image2(image, min_dim=None, max_dim=None, padding=False):
     scale: The scale factor used to resize the image
     padding: Padding added to the image [(top, bottom), (left, right), (0, 0)]
     """
-    # Default window (y1, x1, y2, x2) and default scale == 1.
+    # Default window (y1, x1, y2, x2)
     h, w = image.shape[:2]
     window = (0, 0, h, w)
-    scale = 1
-
-    # Scale?
-    if min_dim:
-        # Scale up but not down
-        scale = max(1, min_dim / min(h, w))
+    
     # Does it exceed max dim?
     if max_dim:
         image_max = max(h, w)
@@ -466,19 +460,19 @@ def resize_image2(image, min_dim=None, max_dim=None, padding=False):
             scale = max_dim / image_max
     # Resize image and mask
     if scale != 1:
-        image = scipy.misc.imresize(
-            image, (round(h * scale), round(w * scale)), interp='nearest')
-    # Need padding?
-    if padding:
-        # Get new height and width
-        h, w = image.shape[:2]
-        top_pad = (max_dim - h) // 2
-        bottom_pad = max_dim - h - top_pad
-        left_pad = (max_dim - w) // 2
-        right_pad = max_dim - w - left_pad
-        padding = [(top_pad, bottom_pad), (left_pad, right_pad), (0, 0)]
-        image = np.pad(image, padding, mode='constant', constant_values=0)
-        window = (top_pad, left_pad, h + top_pad, w + left_pad)
+        image = scipy.misc.imresize(image, (int(round(h * scale)), int(round(w * scale))))
+
+    # Padding
+    # Get new height and width
+    h, w = image.shape[:2]
+    top_pad = (max_dim - h) // 2
+    bottom_pad = max_dim - h - top_pad
+    left_pad = (max_dim - w) // 2
+    right_pad = max_dim - w - left_pad
+    padding = [(top_pad, bottom_pad), (left_pad, right_pad), (0, 0)]
+    image = np.pad(image, padding, mode='constant', constant_values=0)
+    window = (top_pad, left_pad, h + top_pad, w + left_pad)
+
     return image, window, scale, padding
 
 