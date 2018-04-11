8f7701535671fd41801394c4264735c540cd046d
ciararogerson <ciara.rogerson@stcatz.oxon.org>
2018-04-11 13:22:56 +0100

    na

--- Mask_RCNN_DSB2018/DSB2018/train.py
+++ Mask_RCNN_DSB2018/DSB2018/train.py
@@ -234,6 +234,124 @@ def train_resnet101_semantic_b_w_colour(training = True):
         return [bw_config, colour_config], [bw_dataset, colour_dataset], model_name
 
 
+def train_resnet101_semantic_b_w_colour_maskcount_balanced(training = True):
+    """
+    b/w and colour models: 
+    - semantic included
+    - greyscale + invert_type == 2 / 0
+    - original config, with max_gt_instances = 400
+    - fn_load = b/w: load_image_gt_augment; colour: load_image_gt_augment_nsb
+    - rpn_nms_threshold 0.9 in training, 0.7 when submitting
+    """
+
+    model_name = 'BespokeMaskRCNN'
+    bw_dataset_kwargs = { 'invert_type' : 2 , 'cache' : DSB2018_Dataset.Cache.DISK }
+    colour_dataset_kwargs = { 'invert_type' : 0 , 'cache' : DSB2018_Dataset.Cache.DISK }
+
+
+    bw_config = mask_rcnn_config(train_data_root = [train_dir] + supplementary_dir,
+                            test_data_root = [test_dir],
+                            init_with = 'coco',
+                            architecture = 'resnet101',
+                            mini_mask_shape = 12,
+                            max_gt_instances = 400,
+                            rpn_nms_threshold = 0.9,
+                            images_per_gpu = 2, 
+                            identifier = 'semantic_bw_bal',
+                            fn_load = 'load_image_gt_augment_nsb',
+                            augmentation_dict = {'dim_ordering': 'tf',
+                                                'horizontal_flip': True,
+                                                'vertical_flip': True, 
+                                                'rots' : True,
+                                                'gaussian_blur': [-0.2, 0.2]})
+
+
+    colour_config = mask_rcnn_config(train_data_root = [train_dir] + supplementary_dir,
+                            test_data_root = [test_dir],
+                            init_with = 'coco',
+                            architecture = 'resnet101',
+                            mini_mask_shape = 12,
+                            max_gt_instances = 400,
+                            rpn_nms_threshold = 0.9,
+                            images_per_gpu = 1, 
+                            identifier = 'semantic_colour_bal',
+                            fn_load = 'load_image_gt_augment_nsb',
+                            augmentation_dict = {'dim_ordering': 'tf',
+                                                'horizontal_flip': True,
+                                                'vertical_flip': True, 
+                                                'rots' : True,
+                                                'gaussian_blur': [-0.2, 0.2]})
+
+
+    if training:
+        ################
+        # b/w model 
+        ################
+
+        # Training dataset
+        dataset_train = DSB2018_Dataset(**bw_dataset_kwargs)
+        dataset_train.add_nuclei(bw_config.train_data_root, 'train', split_ratio = 1., target_colour_id = np.array([1]))
+        dataset_train.prepare()
+
+        # Validation dataset
+        dataset_val = None
+
+        # Create model in training mode
+        bw_model = getattr(modellib, model_name)(mode="training", config=bw_config,
+                                    model_dir=bw_config.MODEL_DIR)
+        bw_model = load_weights(bw_model, bw_config)
+            
+        bw_model.train(dataset_train, dataset_val,
+                    learning_rate=bw_config.LEARNING_RATE,
+                    epochs=1 if TESTING else 20,
+                    layers='all',
+                    balance_by_cluster_id = True, str_cluster_id = 'maskcount_id')
+            
+        ################
+        # colour model 
+        ################
+
+        # Training dataset
+        dataset_train = DSB2018_Dataset(**colour_dataset_kwargs)
+        dataset_train.add_nuclei(train_dir, 'train', split_ratio = 1., target_colour_id = np.array([2]))
+        for repeats in range(135//45):
+            dataset_train.add_nuclei(supplementary_dir, 'train', split_ratio = 1., target_colour_id = np.array([2]))
+        dataset_train.prepare()
+
+        # Validation dataset
+        dataset_val = None
+
+        # Create model in training mode
+        colour_model = getattr(modellib, model_name)(mode="training", config=colour_config,
+                                    model_dir=colour_config.MODEL_DIR)
+        colour_model = load_weights_from_model(colour_model, bw_model)
+    
+        # Clear bw_model
+        bw_model = None
+
+        colour_model.train(dataset_train, dataset_val,
+                    learning_rate=colour_config.LEARNING_RATE,
+                    epochs=1 if TESTING else 10,
+                    layers='all',
+                    augment_val = True,
+                    balance_by_cluster_id = True, str_cluster_id = 'maskcount_id')
+
+    else:
+
+        bw_dataset = DSB2018_Dataset(**bw_dataset_kwargs)
+        bw_dataset.add_nuclei(test_dir, 'test', shuffle = False, target_colour_id = np.array([1]))
+        bw_dataset.prepare()
+
+        colour_dataset = DSB2018_Dataset(**colour_dataset_kwargs)
+        colour_dataset.add_nuclei(test_dir, 'test', shuffle = False, target_colour_id = np.array([2]))
+        colour_dataset.prepare()
+
+        bw_config.RPN_NMS_THRESHOLD = 0.7
+        colour_config.RPN_NMS_THRESHOLD = 0.7
+
+        return [bw_config, colour_config], [bw_dataset, colour_dataset], model_name
+
+
 def main():
     #train_resnet101_semantic()
     train_resnet101_semantic_b_w_colour()
